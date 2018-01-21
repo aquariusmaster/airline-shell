a5a3539eaac024fc18ab3f625eb3cf619a312c41
Andrey Bobrov <verydrybeaver@gmail.com>
2018-01-21 16:10:15 +0200

    add descriptions to AirlineCLI

--- src/main/java/ua/com/globallogic/airline/service/shell/AirlineCLI.java
+++ src/main/java/ua/com/globallogic/airline/service/shell/AirlineCLI.java
@@ -22,6 +22,12 @@ public class AirlineCLI {
         this.airCraftService = airCraftService;
     }
 
+    /**
+     * Output all Aircrafts.
+     *
+     * @param sorted Sort by flight range if sorted true
+     * @return List of Aircrafts
+     */
     @ShellMethod(value = "Show all aircrafts (optional sorted by flight range)", prefix = "-")
     public String all(@ShellOption(help = "sorted by flight range") boolean sorted){
 
@@ -29,6 +35,15 @@ public class AirlineCLI {
                     .collect(Collectors.joining("\n"));
     }
 
+    /**
+     * Save new Aircrafts.
+     *
+     * @param capacity Capacity of the AirCraft
+     * @param carryingCapacity Carrying Capacity of the AirCraft
+     * @param flightRange Flight Range of the AirCraft
+     * @param fuelConsumption Fuel Consumption of the AirCraft
+     * @return String List of Aircrafts
+     */
     @ShellMethod("Add new aircraft")
     public String add(float capacity,
                       float carryingCapacity,
@@ -44,6 +59,12 @@ public class AirlineCLI {
         return "New aircraft was added: " + airCraftService.saveOrUpdate(airCraft);
     }
 
+    /**
+     * Get Aircraft by id
+     *
+     * @param id AirCraft id
+     * @return String AitCraft that was found
+     */
     @ShellMethod("Get aircraft by id")
     public String get(Long id){
 
@@ -55,6 +76,13 @@ public class AirlineCLI {
         }
     }
 
+    /**
+     * Get Aircraft by id
+     *
+     * @param capacity is need to calc capacity
+     * @param carrying is need to calc carryingCapacity
+     * @return String Total capacity and/or carryingCapacity
+     */
     @ShellMethod(value = "Calculate total capacity and carrying capacity of all the aircraft in the airline",
             prefix = "-")
     public String total(@ShellOption(help = "output total capacity") boolean capacity,
@@ -73,6 +101,12 @@ public class AirlineCLI {
         }
     }
 
+    /**
+     * Find Aircrafts by range of fuel consumption
+     *
+     * @param range range of fuel consumption
+     * @return String List of AirCraft that was found
+     */
     @ShellMethod(value = "Find aircraft corresponding to the specified range of fuel consumption parameters " +
             "(liters per hour)", prefix = "-")
     public String find(@ShellOption(arity = 2, help = "range of fuel consumption") double[] range){
